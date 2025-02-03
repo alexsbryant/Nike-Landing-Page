fd430ce5d4af0ea412300ba77ca2444601644178
Alexander Bryant <asbryant@me.com>
2025-02-03 15:58:38 -0700

    Special Offer section created
    Button edited to allow props for different button styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const Button = ({ label, iconURL }) => {
+const Button = ({ label, iconURL, backgroundColor, textColor, borderColor }) => {
   return (
-    <button className='flex justify-center items-center gap-2 w-50 h-13 px-7 py-4 border font-mont text-lg leading-none bg-red-400 rounded-full text-white border-red-400'>
+    <button className={`flex justify-center items-center gap-2 w-50 h-13 px-7 py-4 border font-mont text-lg leading-none
+      ${backgroundColor} ? 
+      ${backgroundColor} ${textColor} ${borderColor}
+      : bg-red-400 rounded-full text-white border-red-400`}>
         {label}
         {iconURL && <img 
           src={iconURL} 
--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Button from '../components/Button'
 import { offer } from '../assets/images'
+import { arrowRight } from '../assets/icons'
 
 const SpecialOffer = () => {
   return (
@@ -9,16 +10,22 @@ const SpecialOffer = () => {
         <img src={offer} width={773} height={687} className='object-contain w-full'/>
       </div>
       <div className='flex flex-1 flex-col'>
-        <h2 className='mt-10 pal-semibold text-4xl capitalize'>We Provide You
-          <span className='text-coral-red'> Super</span>
-          <br />
-          <span className='text-coral-red'>Quality</span> Shoes
+        <h2 className='mt-10 pal-semibold text-4xl capitalize'>
+          <span className='text-coral-red'> Special</span> Offer
         </h2>
-        <p className='mt-4 lg:max-w-lg font-sans text-slate-500 text-lg leading-7'>Special offer text</p>
-        <p className='mt-6 lg:max-w-lg font-sans text-slate-500 text-lg leading-7'>Special offer text two.</p>
-        <div className='mt-11'>
+        <br />
+        <p className='mt-4 lg:max-w-lg font-sans text-slate-500 text-lg leading-7'>Winter sale items just dropped...</p>
+        <p className='mt-6 lg:max-w-lg font-sans text-slate-500 text-lg leading-7'>Check out our latest sale items, with up to 30% off select styles and colors.  Offer ends March 1st 2025.</p>
+        <div className='mt-11 flex flex-wrap gap-4'>
           <Button
-            label="View Details"
+            label="Shop now"
+            iconURL={arrowRight}
+          />
+          <Button 
+            label="Learn More"
+            backgroundColor="bg-white"
+            borderColor="border-slate-gray"
+            textColor="text-slate-gray"
           />
         </div>
       </div>
